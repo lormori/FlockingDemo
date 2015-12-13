9c7027a9f311e64fd9969c32b561c3bae5f71edd
Lorenzo Mori <lor.mori@gmail.com>
2015-12-13 13:00:29 +0000

    First person view and camera movement implemented

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,15 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraMovement : MonoBehaviour {
+public class CameraMovement : MonoBehaviour
+{
+    private float mCameraSpeed = 5.0f;
 
-	// Use this for initialization
-	void Start () {
+    //-----------------------------------------------------------------------------
+    // Functions
+    //-----------------------------------------------------------------------------
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        UpdateMouseMovement();
+
+        UpdateKeyPressed();
 	}
+
+    private void UpdateKeyPressed()
+    {
+        Vector3 velocity = Vector3.zero;
+
+        float speedCoefficient = ( mCameraSpeed * Time.deltaTime );
+
+        if (Input.GetKey("a"))
+        {
+            velocity += -transform.right * speedCoefficient;
+        }
+
+        if ( Input.GetKey( "d" ) )
+        {
+            velocity += transform.right * speedCoefficient;
+        }
+
+        if ( Input.GetKey( "w" ) )
+        {
+            velocity += transform.forward * speedCoefficient;
+        }
+
+        if ( Input.GetKey( "s" ) )
+        {
+            velocity += -transform.forward * speedCoefficient;
+        }
+
+        transform.position += velocity;
+    }
+
+    private void UpdateMouseMovement()
+    {
+        if(Input.GetMouseButton(1))
+        {
+            Vector3 rotation = transform.eulerAngles;
+
+            rotation.y += Input.GetAxis( "Mouse X" ) * mCameraSpeed;
+            rotation.x -= Input.GetAxis( "Mouse Y" ) * mCameraSpeed;
+
+            transform.rotation = Quaternion.Euler( rotation );
+        }
+    }
 }
